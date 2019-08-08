5535d0a462c681c8b158593deda2c21788add809
Dongmin Kim <kdm951027@gmail.com>
2019-08-08 17:01:31 -0500

    initial commit

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flaskblog import create_app
+# from flaskblog import create_app, db
 
 app = create_app()
+# with app.app_context():
+#     db.create_all()
 
 if __name__ == '__main__':
+
     app.run(debug=True)
